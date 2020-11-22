08660ccc7f8040a9445a54a0481e6fee650f8d0d
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-22 19:42:04 +0900

    Create header components

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import myheader from '@/components/myheader'
+import about from '@/components/about'
+import works from '@/components/works'
+import skill from '@/components/skill'
+import contact from '@/components/contact'
 
 Vue.use(Router)
 
@@ -8,8 +13,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: myheader
+    },
+    {
+      path: '/about',
+      component: about
+    },
+    {
+      path: '/works',
+      component: works
+    },
+    {
+      path: '/skill',
+      component: skill
+    },
+    {
+      path: '/contact',
+      component: contact
     }
   ]
 })
