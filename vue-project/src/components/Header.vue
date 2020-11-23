cac122d6e7edb4f30d938845c55c8c182deab7db
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-23 17:40:08 +0900

    Create Header.vue

---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class= "header">
+      <div class= "left-header">
+        <router-link to="/Home">Aoki Takashi's Portfolio</router-link>
+      </div>
+      <div class= "right-header">
+        <router-link to="/About">About</router-link>
+        <router-link to="/Works">Works</router-link>
+        <router-link to="/Skill">Skill</router-link>
+        <router-link to="/Contact">Contact</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-around;
+
+}
+</style>
