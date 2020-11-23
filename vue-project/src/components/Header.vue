f76f79426da48b846583dcbc24b92057d956b122
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-23 18:17:55 +0900

    added class right-header

--- vue-project/src/components/Header.vue
+++ vue-project/src/components/Header.vue
@@ -4,12 +4,20 @@
       <div class= "left-header">
         <router-link to="/Home">Aoki Takashi's Portfolio</router-link>
       </div>
-      <div class= "right-header">
-        <router-link to="/About">About</router-link>
-        <router-link to="/Works">Works</router-link>
-        <router-link to="/Skill">Skill</router-link>
-        <router-link to="/Contact">Contact</router-link>
-      </div>
+      <ul class="right-header">
+        <li class="About">
+          <router-link to="/About">About</router-link>
+        </li>
+        <li class="Works">
+          <router-link to="/Works">Works</router-link>
+        </li>
+        <li class="Skill">
+          <router-link to="/Skill">Skill</router-link>
+        </li>
+        <li class="Contact">
+          <router-link to="/Contact">Contact</router-link>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
@@ -18,9 +26,22 @@
 </script>
 
 <style>
+header {
+  font-size: 30px;
+}
 .header {
+  height: 50px;
   display: flex;
   justify-content: space-around;
-
+  align-items: center;
+}
+.right-header {
+  display: flex;
+}
+ul {
+  list-style: none;
+}
+li {
+  margin-right: 30px;
 }
 </style>
