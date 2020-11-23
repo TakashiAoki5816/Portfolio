c73c4df0f42a6fbce4e7bfd63cbb436a3c5a4330
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-23 17:43:40 +0900

    Delete routes myheader

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import myheader from '@/components/myheader'
 import about from '@/components/about'
 import works from '@/components/works'
 import skill from '@/components/skill'
@@ -11,10 +10,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      component: myheader
-    },
     {
       path: '/about',
       component: about
