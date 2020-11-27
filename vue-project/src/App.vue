4deb6f552d3c656c0d2c2f255336150516ed5e00
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-27 16:13:46 +0900

    Create Home.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Header></Header>
+    <Home></Home>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Home from './components/Home.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    Home
   }
 }
 </script>
