5de23279cb1e4b166de9a41e015179ab90c77d80
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-28 00:41:45 +0900

    Create footer.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <Header></Header>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/header.vue'
+import Footer from './components/footer.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ vue-project/src/components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <div class= "footer">
+      <p>
+        <span>Aoki Takashi's Portfolio</span> by
+        <a>@arfaben__5816</a></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
