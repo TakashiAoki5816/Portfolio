365f946aff5d525c0d1e3e0229aa6889fe761e6c
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-29 17:34:32 +0900

    Create about.vue

---
+++ vue-project/src/components/about.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="wrapper">
+  <div class="title-header">
+    <div class="title">
+      <h2>About</h2>
+    </div>
+  </div>
+  <div class="left-content">
+
+  </div>
+  <div class="right-content">
+
+  </div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+.title-header {
+  width: 90vw;
+  margin: 0 auto;
+  height: auto;
+  display: flex;
+  justify-content: start;
+}
+
+.title {
+  font-size: 4em;
+}
+
+.left-content {
+
+}
+
+.right-content {
+
+}
+
+</style>
