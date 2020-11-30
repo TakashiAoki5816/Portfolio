1dd8f72b9cc4c769cc7b8a522b1da93e6ecf80a5
Takashi Aoki <arfaben_5816@icloud.com>
2020-11-30 14:01:43 +0900

    Update Profile

--- vue-project/src/components/about.vue
+++ vue-project/src/components/about.vue
@@ -14,12 +14,14 @@
         <div class="right-profile">
           <h4 class="profile-title">Profile</h4>
           <p>はじめまして、青木 貴志と申します。</p>
-          <p>今年、3月に東京農業大学 地域環境科学部 造園科学科を卒業しました。</p>
-          <p>大学4年の時にプログラミングの面白さにハマり、造園ではなくエンジニアを目指すことに決意</p>
-          <p>趣味はサッカー・フットサル・ランニング・自転車・プログラミング・音楽</p>
-          <p>休日はサッカーの大会やランニングなど、基本的に体を動かしています。</p>
+          <p>今年、3月に東京農業大学 地域環境科学部 造園科学科を卒業しました。</p><br>
+          <p>4年生の時にプログラミングの面白さにハマり、<br>造園ではなくエンジニアを目指すことに決意しました。</p><br>
+          <p>趣味は、サッカー・フットサル・ランニング・自転車・プログラミング・音楽</p>
+          <p>休日は、サッカーの大会やランニングなど、基本的に体を動かしています。</p><br>
           <p>去年もIT業界を中心に就職活動を行っていましたが、</p>
-          <p>納得がいかなかったため、もう一度やり直しています。（2020年11月29日現在）</p>
+          <p>納得がいく就職活動ができなかったため、もう一度やり直しています。<br>（2020年11月30日現在）</p><br>
+          <p>現在は日々、ポートフォリオのブラッシュアップ・技術力向上に励んでいます。</p>
+          <p>最近は、Vue.jsでポートフォリオサイトを作成しています。</p>
         </div>
       </div>
       <div class="mytwitter">
@@ -28,8 +30,15 @@
     </div>
     <div class="right-content">
       <h4 class="History">History</h4>
-      <p>2016年3月</p>
-      <p>東京農業大学に入学</p>
+      <ul class="timeline">
+        <li>
+          <p class="timeline-date">2016年3月</p>
+          <div class="timeline-content">
+            <h3>東京農業大学に入学</h3>
+            <p></p>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </div>
@@ -60,21 +69,21 @@
 }
 
 .left-content {
-  width: 60%;
+  width: 50%;
   height: 100%;
-  background-color: chocolate;
 }
 
 .profile {
   height: 50%;
   display: flex;
-  background: rgb(143, 237, 100);
+  background: rgb(140, 235, 145);
+  border-radius: 6px;
+  box-shadow: 0 0.5em 1em -0.125em rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.02);
 }
 
 .left-profile {
-  width: 30%;
+  width: 25%;
   height: 100%;
-  background: crimson;
 }
 
 .profile-img {
@@ -82,14 +91,14 @@
 }
 
 .right-profile {
-  width: 70%;
+  width: 75%;
   height: 100%;
   text-align: left;
-  background: rgb(192, 182, 40);
 }
 
 .profile-title {
   font-size: 3em;
+  margin-bottom: 10px;
 }
 
 .mytwitter {
@@ -103,7 +112,7 @@
 }
 
 .right-content {
-  width: 40%;
+  width: 50%;
   height: 100%;
   background-color: cornflowerblue;
 }
