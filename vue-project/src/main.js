15022945f951414275bde9521355ccf4330d69cb
Takashi Aoki <arfaben_5816@icloud.com>
2020-12-01 21:45:41 +0900

    added font-awesome

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// font-awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
