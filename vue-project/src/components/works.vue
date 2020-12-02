d13ca34109d3a69ef39b7f5d49de39ea3e7f958f
Takashi Aoki <arfaben_5816@icloud.com>
2020-12-02 17:33:34 +0900

    added niwaai-img

--- vue-project/src/components/works.vue
+++ vue-project/src/components/works.vue
@@ -8,16 +8,16 @@
   <div class="content-wrapper">
     <div class="content">
       <div class="content-image">
-
+        <a href="https://www.niwaai.com/" target="_blank" rel="noopener noreferrer"><img src="../assets/niwaai.png"></a>
       </div>
       <div class="content-title">
         <h4>Niwaai</h4>
       </div>
       <div class="content-explanation">
-        <p>あなたのお気に入りの日本庭園に出会おうをコンセプトにした庭園写真共有サイト</p>
+        <p>「あなたのお気に入りの日本庭園に出会おう」をコンセプトにした庭園写真共有サイト</p>
       </div>
       <div class="content-github">
-
+        <a href="https://github.com/TakashiAoki5816/niwaai" target="_blank" rel="noopener noreferrer">Github</a>
       </div>
     </div>
     <div class="content">
@@ -58,5 +58,10 @@
   width: 50%;
   height: 100%;
   background: darkmagenta;
+}
+
+.content-image img {
+  width: 90%;
+  height: auto;
 }
 </style>
