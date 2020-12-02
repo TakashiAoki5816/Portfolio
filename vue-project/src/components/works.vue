5d8ccccf27433c5bc4e21de6c007c056434f4798
Takashi Aoki <arfaben_5816@icloud.com>
2020-12-02 13:58:13 +0900

    added flamework content

--- vue-project/src/components/works.vue
+++ vue-project/src/components/works.vue
@@ -5,6 +5,24 @@
       <h2>Works</h2>
     </div>
   </div>
+  <div class="content-wrapper">
+    <div class="content">
+      <div class="content-image">
+
+      </div>
+      <div class="content-title">
+      </div>
+      <div class="content-explanation">
+
+      </div>
+      <div class="content-github">
+
+      </div>
+    </div>
+    <div class="content">
+
+    </div>
+  </div>
 </div>
 </template>
 
@@ -26,5 +44,18 @@
 
 .title {
   font-size: 4em;
+}
+
+.content-wrapper {
+  width: 100%;
+  height: 80%;
+  background: darkgreen;
+  display: flex;
+}
+
+.content {
+  width: 50%;
+  height: 100%;
+  background: darkmagenta;
 }
 </style>
