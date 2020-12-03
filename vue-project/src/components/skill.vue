a50a2e513be48ba14ababaa218ec6a5fe60dbd8e
Takashi Aoki <arfaben_5816@icloud.com>
2020-12-03 14:35:17 +0900

    added skill-wrapper

---
+++ vue-project/src/components/skill.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="skill-wrapper">
+    <div class="title-header">
+      <div class="title">
+        <h2>Skill</h2>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style>
+.skill-wrapper {
+  width: 90vw;
+  height: 80vh;
+  margin: 10px auto;
+  background-color: darkorange;
+}
+</style>
