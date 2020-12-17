274301fbba1bd6fda4f533c4aa60bd72821fc523
Takashi Aoki <arfaben_5816@icloud.com>
2020-12-17 17:30:44 +0900

    Create contact.vue

--- vue-project/src/components/contact.vue
+++ vue-project/src/components/contact.vue
@@ -5,6 +5,12 @@
         <h2>Contact</h2>
       </div>
     </div>
+    <img src="../assets/profile-img.png" class="contact-img">
+    <div class="contact-content">
+      <p>ここまでポートフォリオサイトを見てくださり、ありがとうございます。</p>
+      <p>もし、ご連絡・ご用件等ありましたらTwitterのDMまでよろしくお願いします。</p>
+      <p><a href="https://twitter.com/arfaben__5816" class="bluelink" target="_blank" rel="noopener noreferrer">Twitter</a></p>
+    </div>
   </div>
 </template>
 
